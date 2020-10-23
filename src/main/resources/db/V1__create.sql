27016f66622fb48ad25711584b4728a407ce9302
Oksana Misharina <69849607+OksanaMisharina@users.noreply.github.com>
2020-10-23 16:29:28 +0300

    Update V1__create.sql

--- src/main/resources/db/V1__create.sql
+++ src/main/resources/db/V1__create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public."TEACHER"
 (
-    "TEACHER_ID" integer NOT NULL,
+    "TEACHER_ID" serial NOT NULL,
     "SURNAME" character varying(50) COLLATE pg_catalog."default" NOT NULL,
     "NAME" character varying(50) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "TEACHER_pkey" PRIMARY KEY ("TEACHER_ID")
@@ -8,14 +8,14 @@ CREATE TABLE public."TEACHER"
 
 CREATE TABLE public."SUBJECT"
 (
-    "SUBJ_ID" integer NOT NULL,
+    "SUBJ_ID" serial NOT NULL,
     "SUBJ_NAME" character varying(100) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "SUBJECT_pkey" PRIMARY KEY ("SUBJ_ID")
 );
 
 CREATE TABLE public."STUDENT"
 (
-    "STUD_ID" integer NOT NULL,
+    "STUD_ID" serial NOT NULL,
     "SURNAME" character varying(50) COLLATE pg_catalog."default" NOT NULL,
     "NAME" bit varying(50) NOT NULL,
     CONSTRAINT "STUDENT_pkey" PRIMARY KEY ("STUD_ID")
